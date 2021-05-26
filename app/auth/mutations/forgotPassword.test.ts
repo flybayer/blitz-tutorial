d72f2b46f668af21e045ffb1068f938356064677
Brandon Bayer <b@bayer.ws>
2021-05-26 16:09:05 -0500

    fix typecheck

--- app/auth/mutations/forgotPassword.test.ts
+++ app/auth/mutations/forgotPassword.test.ts
@@ -41,12 +41,12 @@ describe("forgotPassword mutation", () => {
     await forgotPassword({ email: user.email }, {} as Ctx)
 
     const tokens = await db.token.findMany({ where: { userId: user.id } })
-    const token = tokens[0]
+    const token = tokens[0]!
 
     // delete's existing tokens
     expect(tokens.length).toBe(1)
 
-    expect(token.id).not.toBe(user.tokens[0].id)
+    expect(token.id).not.toBe(user.tokens[0]!.id)
     expect(token.type).toBe("RESET_PASSWORD")
     expect(token.sentTo).toBe(user.email)
     expect(token.hashedToken).toBe(hash256(generatedToken))
